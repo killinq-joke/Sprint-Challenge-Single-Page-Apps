3bf3d70cb318aae2323eb546986038653914ddc4
Zak <zakadmaster@gmail.com>
2020-01-31 09:34:30 +0100

    Addition routes, and character page looping through api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Route } from 'react-router-dom';
 import Header from "./components/Header.js";
+import axios from "axios";
+import CharacterList from './components/CharacterList'
 
 
 export default function App() {
+  // const [characters, setCharacters] = useState([])
+
+  // useEffect(() => {
+  //   axios
+  //   .get(`https://rickandmortyapi.com/api/character/`)
+  //   .then(res => {
+  //     console.log(res.data.results)
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //   })
+  // }, [])
+
   return (
     <main>
-      <Header />
+      <Route exact path="/">
+        <Header />
+      </Route>
+      
+      <Route exact path="/characters">
+        <CharacterList />
+      </Route>
     </main>
   );
 }
